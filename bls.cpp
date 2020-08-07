0f73778e4c8d844ae7bae0a170295b2ae2109f95
trytds <1307805140@qq.com>
2020-08-07 11:20:37 +0800

    fix

--- bls.cpp
+++ bls.cpp
@@ -9,7 +9,7 @@
 
    For MR_PAIRING_MNT curve
    cl /O2 /GX bls.cpp mnt_pair.cpp zzn6a.cpp ecn3.cpp zzn3.cpp zzn2.cpp big.cpp zzn.cpp ecn.cpp miracl.lib
-	
+
    For MR_PAIRING_BN curve
    cl /O2 /GX bls.cpp bn_pair.cpp zzn12a.cpp ecn2.cpp zzn4.cpp zzn2.cpp big.cpp zzn.cpp ecn.cpp miracl.lib
 
@@ -19,7 +19,7 @@
    For MR_PAIRING_BLS curve
    cl /O2 /GX bls.cpp bls_pair.cpp zzn24.cpp zzn8.cpp zzn4.cpp zzn2.cpp ecn4.cpp big.cpp zzn.cpp ecn.cpp miracl.lib
 
-   Test program 
+   Test program
 */
 
 #include <iostream>
@@ -44,56 +44,72 @@
 //*********************************************
 
 #include "pairing_3.h"
-int main()
-{   
-	PFC pfc(AES_SECURITY);  // 初始化双线性对曲线
-	G1 S,R;
-;	G2 Q,V;
-	int lsb;
-	Big s,X;
-	time_t seed;
-	const char* message = "Be there or be square!";  //待签名消息m
-	time(&seed);
-    irand((long)seed);
 
+PFC pfc(AES_SECURITY);  // 初始化双线性对曲线
+Big s;
+G1 S, R;
+G2 Q, V;
+Big X;
 
-// Create system-wide G2 constant
+void keygenBLS()
+{
+	//G2 Q, V;
+    //Big s;
+	// Create system-wide G2 constant
 	pfc.random(Q);
 
 	pfc.random(s);    // private key
-	V=pfc.mult(Q,s);  // public key
+	printf("privatekey: %d\n", s);
+	V = pfc.mult(Q, s);  // public key
+	printf("publickey: %d\n", V);
 
-// signature
-	pfc.hash_and_map(R,(char*)message);
-	S=pfc.mult(R,s);
+}
 
-	lsb=S.g.get(X);   // signature is lsb bit and X
+void signBLS(unsigned char *plain,int *lsb)
+{
+    //G1 S, R;
+    //Big X;
+    pfc.hash_and_map(R, (char*)plain);
+    S = pfc.mult(R, s);
 
-	cout << "Signature= " << lsb << " " << X << endl;
+    *lsb = S.g.get(X);   // signature is lsb bit and X
 
-// verification	- first recover full point S
-	if (!S.g.set(X,1-lsb))
-	{
-		cout << "Signature is invalid" << endl;
-		exit(0);
-	}
-	pfc.hash_and_map(R,(char *)message);
+    cout << "Signature= " << lsb << " " << X << endl;
+}
+
+void vertifyBLS(unsigned char *plain, int *lsb)
+{
+    if (!S.g.set(X, 1 - *lsb))
+    {
+        cout << "Signature is invalid" << endl;
+        exit(0);
+    }
+    pfc.hash_and_map(R, (char*)plain);
 
 
-// Observe that Q is a constant
-// Interesting that this optimization doesn't work for the Tate pairing, only the Ate
+    // Observe that Q is a constant
+    // Interesting that this optimization doesn't work for the Tate pairing, only the Ate
 
-	pfc.precomp_for_pairing(Q);
+    pfc.precomp_for_pairing(Q);
 
-	G1 *g1[2];
-	G2 *g2[2];
-	g1[0]=&S; g1[1]=&R;
-	g2[0]=&Q; g2[1]=&V;
+    G1* g1[2];
+    G2* g2[2];
+    g1[0] = &S; g1[1] = &R;
+    g2[0] = &Q; g2[1] = &V;
 
-	if (pfc.multi_pairing(2,g2,g1)==1)
-		cout << "Signature verifies" << endl;
-	else
-		cout << "Signature is bad" << endl;
+    if (pfc.multi_pairing(2, g2, g1) == 1)
+        cout << "Signature verifies" << endl;
+    else
+        cout << "Signature is bad" << endl;
+}
 
-    return 0;
+int main()
+{
+    unsigned char tx[5000] = "Be there or be square!"; //待签名明文
+    int etx[4000]; //签名信息
+    unsigned char mtx[5000] = "0"; //解密
+	keygenBLS();
+    signBLS(tx,etx);
+    vertifyBLS(mtx,etx);
+	return 0;
 }
